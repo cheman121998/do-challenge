1e9d98f853c80df34db6a364258132daefd0d50e
Man Man <chethiminhman121998@gmail.com>
2020-08-25 06:26:06 +0700

    Update 2 challenge run on web

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
+import ChallengeOne from "./pages/ChallengeOne"; //Vì thằng app.js import cả 2 thằng
+import ChallengeTwo from "./pages/ChallengeTwo"; //Vì thằng app.js import cả 2 thằng dạmmm
+import './App.css' //Những style dùng chung e phải đặt ở trong này 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Man Man
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ 
+  render(){
+    return (
+      <BrowserRouter basename={'/do-challenge'} >
+    
+         <Switch>
+           <Route exact path="/1">
+             <ChallengeOne />
+           </Route>
+
+
+           <Route exact path='/2'>
+             <ChallengeTwo />
+           </Route>
+
+         </Switch>
+       
+     </BrowserRouter>
+    );
+  }
 }
 
+
+
 export default App;
+
